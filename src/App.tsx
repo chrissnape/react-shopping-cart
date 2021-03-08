abb8a19b6c713ee53c55fbe9c103179b642d034b
Chris Snape <chris.snape@lovedby.com>
2021-03-08 15:13:59 +0000

    Add extra typing and tidy up in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import { useState } from 'react';
-import './App.css';
-
 import Item from './components/Item';
 import { Basket, Product } from './utils/types';
+import './App.css';
 
 const products: Array<Product> = [
   {id: 'A99', name: 'Apple', price: 50, offer: {price: 130, amount: 3}},
@@ -11,48 +10,47 @@ const products: Array<Product> = [
   {id: 'T34', name: 'Tangerine', price: 99, offer: null},
 ];
 
-function App() {
-  const [basket, updateBasket] = useState<Basket>([]);
+const getTotal = (basket: Array<string>) => {
+  let total: number = 0;
+  const talliedIds: Array<Array<string>> = (
+    products.map((product: Product): Array<string> => (
+      basket.filter((item: string): boolean => item === product.id)
+    ))
+  );
 
-  const getTotal = () => {
-    let total: number = 0;
-    const talliedIds: Array<Array<string>> = (
-      products.map((product: Product): Array<string> => (
-        basket.filter((item: string): boolean => item === product.id)
-      ))
-    );
-
-    talliedIds.forEach((talliedId: Array<string>) => {
-      let tally: number = 0;
-      talliedId.forEach((id: string, i: number) => {
-        const product: Product | undefined = products.find((p: Product) => p.id === id);
-        if (product) {
-          const { offer, price } = product;
-          if (offer) {
-            const { price: offerPrice, amount } = offer;
-            if (tally < amount - 1) {
-              tally ++;
-              if (i === talliedId.length -1) {
-                total += tally * price;
-              }
-            } else {
-              tally = 0;
-              total += offerPrice;
+  talliedIds.forEach((talliedId: Array<string>) => {
+    let tally: number = 0;
+    talliedId.forEach((id: string, i: number) => {
+      const product: Product | undefined = products.find((p: Product) => p.id === id);
+      if (product) {
+        const { offer, price } = product;
+        if (offer) {
+          const { price: offerPrice, amount } = offer;
+          if (tally < amount - 1) {
+            tally ++;
+            if (i === talliedId.length -1) {
+              total += tally * price;
             }
           } else {
-            total += price;
+            tally = 0;
+            total += offerPrice;
           }
+        } else {
+          total += price;
         }
-      });
+      }
     });
+  });
 
-    return total;
-  };
+  return total;
+};
 
+const App = (): JSX.Element => {
+  const [basket, updateBasket] = useState<Basket>([]);
   return (
     <div className="App">
       <div className="app__product-grid">
-        {products.map((product: Product, i: number) => (
+        {products.map((product: Product, i: number): JSX.Element => (
           <div className="app__product-grid__item">
             <Item
               key={`product-${i}`}
@@ -63,13 +61,17 @@ function App() {
         ))}
       </div>
       <div>
-        {basket.map((id: string, i: number) => {
+        {basket.map((id: string, i: number): JSX.Element => {
           const product: Product | undefined = products.find((p: Product) => p.id === id);
           return (
             <div key={`basket-item=${i}`}>
               {product ? product.name : 'undefined'}
               {product ? product.price : ''}
-              <button onClick={() => updateBasket(basket.filter((item: string, j: number) => j !== i))}>
+              <button
+                onClick={() => (
+                  updateBasket(basket.filter((item: string, j: number): boolean => j !== i))
+                )}
+              >
                 delete
               </button>
             </div>
@@ -78,7 +80,7 @@ function App() {
       </div>
       <h4>
         Total
-        {getTotal()}
+        {getTotal(basket)}
       </h4>
     </div>
   );
